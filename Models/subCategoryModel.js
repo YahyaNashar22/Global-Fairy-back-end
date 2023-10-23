5f973e4c4ee64197649b0e4dd8c27cfdaa721c91
Rachwan <rachwan.harb2023@gmail.com>
2023-10-23 09:20:42 +0300

    Fix the id property in subCategory file

--- Models/subCategoryModel.js
+++ Models/subCategoryModel.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 const subCategorySchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    _id: {
+        type: Number,
+    },
     name: {
         type: String,
         required: true,
